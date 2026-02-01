a126697d14fc6624e204cf91837e78c1b2a7bb5e
LongAloso <151179556+LongAloso@users.noreply.github.com>
2026-02-01 19:14:32 +0700

    Update env mongoDB

--- models/User.js
+++ models/User.js
@@ -4,11 +4,11 @@ const userSchema = new mongoose.Schema({
     _id: {type: String, require: true},
     name: {type: String, require: true},
     email: {type: String, require: true, unique:true},
-    imgaeUrl: {type: String, require: true},
+    imageUrl: {type: String, require: true},
     cartItems: {type: Object, default: {} },
 
 }, { minimize: false}) 
 
-const User = mongoose.model.user || mongoose.model('user', userSchema )
+const User = mongoose.models.user || mongoose.model('user', userSchema )
 
 export default User
